a5d6e90ab0b82a5a0411e520650f7247ca76ed82
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-08 13:49:43 -0600

    rename sql database

--- customer.js
+++ customer.js
@@ -3,8 +3,12 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host    : 'localhost',
-    port    : 3306
+    port    : 3306,
     user    : 'root',
     password: 'Superdave214',
-    database: 'my_db'
+    database: 'bamazon_db'
 });
+
+connection.connect(function(err){
+    console.log("Connected as id: "+connection.threadId);
+})
