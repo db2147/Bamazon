232bc04f908817b0a6ad50fb282952d238ee0d69
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-09 18:20:06 -0600

    adding items

--- customer.js
+++ customer.js
@@ -11,4 +11,86 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
     console.log("Connected as id: "+connection.threadId);
+    start();
 })
+
+function postAuction() {
+    // prompt for info about what is for sale
+    inquirer
+        .prompt([
+        {
+            id   : 1,
+            name : "gloves",
+            price: 10.00,
+        },
+        {
+            id   : 2,
+            name : "scissors",
+            price: 4.00,
+        },
+        {
+            id   : 3,
+            name : "boots",
+            price: 59.00,
+        },
+        {
+            id   : 4,
+            name : "cooler",
+            price: 10.00,
+        },
+        {
+            id   : 5,
+            name : "echo dot",
+            price: 30.00,
+        },
+        {
+            id   : 6,
+            name : "Printer",
+            price: 1500.00,
+        },
+        {
+            id   : 7,
+            name : "Handful of Keys",
+            price: 10.00,
+        },
+        {
+            id   : 8,
+            name : "Ball",
+            price: 56.00,
+        },
+        {
+            id   : 9,
+            name : "parsley",
+            price: 20.00,
+        },
+        {
+            id   : 10,
+            name : "sanpellegrino",
+            price: 10.00,
+        },
+    ])
+}
+
+        validate: function(value) {
+            if (isNaN(value) === false) {
+            return true;
+            }
+            return false;
+        }
+      }
+    ])
+        .then(function(answer) {
+        // when finished prompting, insert a new item into the db with that info
+        connection.query(
+        "INSERT INTO auctions SET ?",
+        {
+            item_name: answer.item,
+            category: answer.category,
+            starting_bid: answer.startingBid,
+            highest_bid: answer.startingBid
+        },
+        function(err) {
+            if (err) throw err;
+            console.log("Your auction was created successfully!");
+            // re-prompt the user for if they want to bid or post
+            start();
