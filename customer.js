c09a9156cceede394ad8eb768712dbb43f1bbde0
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-13 14:32:09 -0600

    revisions

--- customer.js
+++ customer.js
@@ -17,6 +17,62 @@ connection.connect(function(err){
 
 function start() {   // defined the start function
     // prompt for info about what is for sale
+    console.log("!!!!!!!!!!!!!!!!!WELCOME TO BAMAZON!!!!!!!!!!!!!!!!!!!!!!");
+    console.log("HERE IS OUR PRODUCT LIST AND INVENTORY/N");
+    connection.query(
+        "SELECT * FROM products",
+        function (err) throw err;
+            dataArr = data;
+            data.forEach(function (val, index) {
+                console.log("ID:" + data[index].item_id + " PRODUCT:" + data[index].product_name + "PRICE:" + 
+            data[index].price + " QUANTITY:" + data[index].stock_quantity);
+                           console.log("---------------------------------------------------");
+            });
+            console.log("/n");
+            shopping();
+        });
+
+    }
+
+    function shopping() { // defined the start function 
+        // prompt for info about what is for sale
+        inquirer
+            .prompt([{
+                type: "list",
+                name: "choice",
+                message: "What is the name of the item you would you like to purchase?",
+                choices: ["gloves",
+                    "cooler",
+                    "echo dot",
+                    "Printer",
+                    "football",
+                    "parsley"
+                ]
+            }]).then(function (answer) {
+                // when finished prompting, insert a new item into the db with that info 
+                console.log(answer.choice);
+                
+                inquirer
+                    .prompt([{
+                        type: "input",
+                        name: "num",
+                        message: "How many would you like to purchase?"
+                    }]).then(function (number) {
+                        console.log(number.num);
+                        // calculate quantity times cost of product and then display it to user
+                           // updating the sql table to account for the product purchased
+                    });
+            });
+
+        }
+    
+    
+    
+    
+    
+    
+    
+    
     inquirer
         .prompt([
         {
